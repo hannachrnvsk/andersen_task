24d58988575e92ef43e027682459e371ccae86f3
hannacrnvsk <4ernyavskaya.aa@gmail.com>
2022-08-10 11:56:50 +0200

    Completed test task

--- task_3_second_version.py
+++ task_3_second_version.py
@@ -1,10 +1,7 @@
 def third_task():
     """Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3"""
     arr = list(map(int, input("Введите массив чисел: ").split()))
-    new_arr = []
-    for i in arr:
-        if i % 3 == 0:
-            new_arr.append(i)
+    new_arr = [i for i in arr if i % 3 == 0]
     print(new_arr)
 
 
